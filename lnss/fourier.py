f5edf72dd004c7cc43abc35a9d92b89e38679aa1
yehuohan <yehuohan@qq.com>
2019-10-06 23:31:01 +0800

    feat(ln-ss): add dfs and idfs

--- lnss/fourier.py
+++ lnss/fourier.py
@@ -2,12 +2,17 @@
 # -*- coding: utf-8 -*-
 
 """
-Fourier（傅里叶）相关算法。
+Fourier（傅里叶）变换相关计算。
+
+此模块只适用于一维信号，以下4个计算基于sympy符号运算库实现（直接套公式）：
 
 - FS(Fourier Series)
 - FT(Fourier Transform)
 - DFS(Discrete Time Fourier Series)
 - DTFT(Discrete Time Fourier Transform)
+
+以下2个变换计算基于numpy运算库实现：
+
 - DFT(Discrete Fourier Transform)
 - FFT(Fast Fourier Transform)
 """
@@ -15,62 +20,134 @@ Fourier（傅里叶）相关算法。
 import numpy as np
 import sympy as sy
 
-def fs(f, T:float, r:tuple=()):
-    """计算傅里叶级数（一维信号）
+def fs(ft, T:float, r:tuple=()):
+    """傅里叶级数
 
-    fs用于连续周期信号，这里基于符号运算库sympy进行计算。
-
-    示例：
+    示例（周期矩形波）：
 
     ..  code:: python
 
-        f = Piecewise((0, t < -0.5), (1, (-0.5 <= t) & (t <= 0.5)), (0, t > 0.5))
-        (a0, an, bn, fn) = fs(f, 2, (t, -1, 1))
+        ft = Piecewise((0, t < -0.5), (1, (-0.5 <= t) & (t <= 0.5)), (0, t > 0.5))
+        (a0, an, bn, fn) = fs(ft, 2, (t, -1, 1))
         print('a0 : ', a0.evalf())
         print('a1 : ', an.subs(n, 1).evalf())
         print('b1 : ', bn.subs(n, 1).evalf())
         print('f1 : ', Abs(fn.subs(n, 1).evalf()))
 
     :Parameters:
-        - f: 基于sympy的周期连续函数表示，函数只能有1个自变量
+        - ft: 连续周期函数，函数只能有1个自变量
         - T: 函数周期
         - r: 函数一个周期的范围
 
     :Returns: Fourier级数系数表达式，自变量符号为n
     """
-    t = tuple(f.free_symbols)[0]    # 函数自变量符号
+    t = tuple(ft.free_symbols)[0]    # 函数自变量符号
     if not r:
         r = (t, -T/2.0, T/2.0)      # 函数周期范围，积分范围
     n = sy.symbols('n')             # 级数自变量
     w = 2.0 * sy.pi / T
-    a0 = 1.0 / T * sy.integrate(f, r)
-    an = 2.0 / T * sy.integrate(f * sy.cos(n * w * t), r)
-    bn = 2.0 / T * sy.integrate(f * sy.sin(n * w * t), r)
-    fn = 1.0 / T * sy.integrate(f * sy.exp(-sy.I * n * w * t), r)
+    a0 = 1.0 / T * sy.integrate(ft, r)
+    an = 2.0 / T * sy.integrate(ft * sy.cos(n * w * t), r)
+    bn = 2.0 / T * sy.integrate(ft * sy.sin(n * w * t), r)
+    fn = 1.0 / T * sy.integrate(ft * sy.exp(-sy.I * n * w * t), r)
     return (a0, an, bn, fn)
 
-def ft(f, r:tuple=()):
+def ft(ft):
     """傅里叶变换
 
-    :Parameters:
+    示例（矩形脉冲波）：
 
-    :Returns:
+    ..  code:: python
+
+        tau = 1
+        ft = Piecewise((0, t < -tau/2.0), (1, (-tau/2.0 <= t) & (t <= tau/2.0)), (0, t > tau/2.0))
+        fw = fourier.ft(ft)
+        print(Abs(fw.subs(w, 0).evalf()))
+
+    :Parameters:
+        - ft: 连续非周期函数，函数只能有1个自变量
+
+    :Returns: 频谱密度函数，自变量符号为omega(用w表示)
     """
-    t = tuple(f.free_symbols)[0]    # 函数自变量符号
+    t = tuple(ft.free_symbols)[0]
     w = sy.symbols('w')
-    if not r:
-        r = (t, -sy.oo, sy.oo)
-    fw = sy.integrate(f * sy.exp(-sy.I * w * t), r)
+    r = (t, -sy.oo, sy.oo)
+    fw = sy.integrate(ft * sy.exp(-sy.I * w * t), r)
     return fw
 
-def ift(f):
-    pass
+def ift(fw):
+    """傅里叶逆变换
 
-def dfs():
-    pass
+    :Parameters:
+        - fw: 频谱密度函数（连续非周期函数），函数只能有1个自变量
 
-def dtft():
-    pass
+    :Returns: 返回原函数，函数自变量变为t
+    """
+    w = tuple(fw.free_symbols)[0]
+    t = sy.symbols('t')
+    r = (w, -sy.oo, sy.oo)
+    ft = (1.0 / 2 * sy.pi) * sy.integrate(fw * sy.exp(sy.I * w * t), r)
+    return ft
+
+def dfs(xn, N:int, r:tuple=()):
+    """离散傅里叶级数
+
+    示例（三角波）：
+
+    ..  code:: python
+
+        N = 21
+        xn = Piecewise((n/10+1, n < 0), (1-n/10, n >= 0))
+        xk = fourier.dfs(xn, N, (n, -(N//2), N//2))
+        print(Abs(xk.subs(k, 1).evalf()))
+
+    :Parameters:
+        - xn: 离散周期序列，序列只能有1个自变量
+        - N: 序列最小数字周期
+        - r: 序列一个周期的范围
+
+    :Returns: 频谱序列（离散周期序列），自变量为k
+    """
+    n = tuple(xn.free_symbols)[0]
+    k = sy.symbols('k')
+    if not r:
+        r = (n, 0, N-1)
+    W = sy.exp(-sy.I * 2 * sy.pi * k * n / N)
+    xk = sy.Sum(xn * W, r)
+    return xk
+
+def idfs(xk, N:int, r:tuple=()):
+    """离散傅里叶级数逆运算
+
+    :Parameters:
+        - xk: 离散周期频谱序列，序列只能有1个自变量
+        - N: 序列最小数字周期
+        - r: 序列一个周期的范围
+
+    :Returns: 原信号序列（离散周期序列），序列自变量为n
+    """
+    k = tuple(xk.free_symbols)[0]
+    n = sy.symbols('n')
+    if not r:
+        r = (k, 0, N-1)
+    W = sy.exp(sy.I * 2 * sy.pi * k * n / N)
+    xn = sy.Sum(xk * W, r) / N
+    return xn
+
+def dtft(xn):
+    """离散时间傅里叶变换
+
+    :Parameters:
+        - xn: 离散非周期序列，序列只能有1个自变量
+
+    :Returns: 序列频谱（周期连续信号），自变量为Omega(用W表示)
+    """
+    n = tuple(xn.free_symbols)[0]
+    W = sy.symbols('W')
+
+def idtft(xW):
+    W = tuple(xn.free_symbols)[0]
+    n = sy.symbols('n')
 
 def dft():
     pass
--- test/fourier-test.py
+++ test/fourier-test.py
@@ -11,10 +11,10 @@ sys.path.append(os.getcwd() + '/../')
 
 import lnss.fourier as fourier
 import numpy as np
-from sympy import Piecewise, integrate, fourier_series, symbols
-from sympy import exp, cos, sin, pi, I, Abs, oo
+from sympy import Piecewise, integrate, fourier_series, symbols, DiracDelta
+from sympy import Sum, exp, cos, sin, pi, I, Abs, oo
 from sympy.plotting import plot
-from sympy.abc import t, n
+from sympy.abc import t, w, n, k
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import moviepy.editor as mpy
@@ -28,36 +28,73 @@ def on_key(event:mpl.backend_bases.KeyEvent):
         plt.close()
 
 #%% FS
+"""
 # 周期脉冲函数，周期T=2，脉宽=T/2，脉冲高度=1
 # 周期增大时，频谱越来越密集
 T = 2
 f = Piecewise((0, t < -T/4.0), (1, (-T/4.0 <= t) & (t <= T/4.0)), (0, t > T/4.0))
 (a0, an, bn, fn) = fourier.fs(f, T)
-# print('a0 : ', a0.evalf())
-# print('an : ', [an.subs(n, k).evalf() for k in range(1, 5)])
-# print('bn : ', [bn.subs(n, k).evalf() for k in range(1, 5)])
-# print('fn : ', [Abs(fn.subs(n, k).evalf()) for k in range(5)])
-# print('Ref: ', [Abs((sin(k * pi / 2) / (k * pi)).evalf()) for k in range(5)])
+print('a0 : ', a0.evalf())
+print('an : ', [an.subs(n, j).evalf() for j in range(1, 5)])
+print('bn : ', [bn.subs(n, j).evalf() for j in range(1, 5)])
+print('fn : ', [Abs(fn.subs(n, j).evalf()) for j in range(5)])
+print('Ref: ', [Abs((sin(j * pi / 2) / (j * pi)).evalf()) for j in range(5)])
 fig = plt.figure('Fourier series')
 fig.canvas.mpl_connect('key_press_event', on_key)
 ax = fig.add_subplot(1, 2, 1)
-ax.plot(np.linspace(-5*T, 5*T, 500), [f.subs(t, k) for k in np.linspace(-T/2, T/2, 50)]*10)
+ax.plot(np.linspace(-T/2*11, T/2*11, 550), [f.subs(t, j) for j in np.linspace(-T/2, T/2, 50)]*11)
 ax = fig.add_subplot(1, 2, 2)
-fn_range = [k for k in range(-5*T, 5*T)]
-fn_points = [Abs(fn.subs(n, k).evalf()) for k in fn_range]
+fn_range = np.arange(-5*T, 5*T + 1)
+fn_points = [Abs(fn.subs(n, j).evalf()) for j in fn_range]
 ax.plot(fn_range, fn_points, 'o')
-for k in fn_range:
-    ax.plot([k, k], [0, fn_points[k+5*T]], 'g', linewidth=2)
+for i,j in enumerate(fn_range):
+    ax.plot([j, j], [0, fn_points[i]], 'g', linewidth=2)
 ax.set_xlabel(r'$|F(n)|-n\omega$')
+"""
+
+#%% FT
+"""
+# 矩形脉冲函数，脉宽tau=1，脉冲高度=1
+tau = 1
+f = Piecewise((0, t < -tau/2.0), (1, (-tau/2.0 <= t) & (t <= tau/2.0)), (0, t > tau/2.0))
+fw = fourier.ft(f)
+print('fw : ', [Abs(fw.subs(w, j).evalf()) for j in range(5)])
+print('Ref: ', [Abs((tau * sin(j * tau / 2) / (j * tau / 2)).evalf()) for j in range(5)])
+fig = plt.figure('Fourier transform')
+fig.canvas.mpl_connect('key_press_event', on_key)
+f_range = np.linspace(-tau*15, tau*15, 100)
+ax = fig.add_subplot(1, 2, 1)
+ax.plot(f_range, [f.subs(t, j) for j in f_range])
+ax = fig.add_subplot(1, 2, 2)
+ax.plot(f_range, [Abs(fw.subs(w, j).evalf()) for j in f_range])
+ax.set_xlabel(r'$|F(\omega)|-\omega$')
+"""
+
+#%% DFS
+# 三角波序列，数字周期N=21
+N = 21
+xn = Piecewise((n/10+1, n < 0), (1-n/10, n >= 0))
+xk = fourier.dfs(xn, N, (n, -(N//2), N//2))
+print('xn : ', [xn.subs(n, j).evalf() for j in range(5)])
+print('xk : ', [Abs(xk.subs(k, j).evalf()) for j in range(5)])
+fig = plt.figure('Discrete Fourier Series')
+fig.canvas.mpl_connect('key_press_event', on_key)
+x_range = np.arange(-(3*N//2), (3*N)//2+1)
+ax = fig.add_subplot(1, 2, 1)
+xn_points = [xn.subs(n, j).evalf() for j in np.arange(-(N//2), N//2+1)]*3
+ax.plot(x_range, xn_points, 'o')
+for i,j in enumerate(x_range):
+    ax.plot([j, j], [0, xn_points[i]], 'g', linewidth=2)
+ax = fig.add_subplot(1, 2, 2)
+xk_points = [Abs(xk.subs(k, j).evalf()) for j in x_range]
+ax.plot(x_range, xk_points , 'o')
+for i,j in enumerate(x_range):
+    ax.plot([j, j], [0, xk_points[i]], 'g', linewidth=2)
+ax.set_xlabel(r'$|X(k)|-k$')
+
+
+#%% DTFT
+# fig = plt.figure('Discrete Time Fourier Transform')
 
 plt.show()
 sys.exit()
-
-#%% FT
-fig = plt.figure('Fourier transform')
-fig.canvas.mpl_connect('key_press_event', on_key)
-ax = fig.add_subplot(1, 2, 1)
-# ax.plot(np.linspace(-5, 5, 500), [f.subs(t, k) for k in np.linspace(-1, 1, 50)]*10)
-ax = fig.add_subplot(1, 2, 2)
-# ax.plot(fn_range, fn_points, 'o')
-# ax.set_xlabel(r'$|F(n)|-n\omega$')
